fb19bb30e8ccb0294e61b186d00734e730063d12
Chandrakant Rathod <cmrathod64@gmail.com>
2026-07-28 13:33:49 +0530

    Completed department data import and other documents master database

--- src/components/DepartmentCard.jsx
+++ src/components/DepartmentCard.jsx
@@ -1,18 +1,22 @@
 import { Link } from "react-router-dom";
 
-function DepartmentCard({ icon, title }) {
-  return (
-    <Link to={`/search?department=${encodeURIComponent(title)}`}>
-      <div className="bg-white p-6 rounded-xl shadow-md hover:shadow-xl hover:-translate-y-2 transition duration-300 text-center cursor-pointer">
+function DepartmentCard({ icon, title, value }) {
+  const searchValue = value || title;
 
-        <div className="text-5xl">
-          {icon}
-        </div>
+  return (
+    <Link
+      to={
+        searchValue === "other"
+          ? "/other-documents"
+          : `/search?department=${encodeURIComponent(searchValue)}`
+      }
+    >
+      <div className="bg-white p-6 rounded-xl shadow-md hover:shadow-xl hover:-translate-y-2 transition duration-300 text-center cursor-pointer">
+        <div className="text-5xl">{icon}</div>
 
         <h2 className="mt-4 text-lg font-bold">
           {title}
         </h2>
-
       </div>
     </Link>
   );
