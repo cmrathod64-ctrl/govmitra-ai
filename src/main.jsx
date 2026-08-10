404e6c9311b5ff83b45a4a27e85309ba8f6e65b9
Chandrakant Rathod <cmrathod64@gmail.com>
2026-08-10 11:11:39 +0530

    Add Vercel Web Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { registerSW } from "virtual:pwa-register";
+import { Analytics } from "@vercel/analytics/react";
 import "./index.css";
 import App from "./App";
 
@@ -13,6 +14,7 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      <Analytics />
     </BrowserRouter>
   </StrictMode>
 );
